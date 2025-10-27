477481a4966325a12e730298bdc73892ff8cf759
Alejandro4092 <acobomarcos@gmail.com>
2025-10-27 14:15:53 +0100

    Hijo deporte

---
+++ src/components/HijoDeporte.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{nombredeporte}}</h1>
+</template>
+
+<script>
+export default {
+ name: "HijoDeporte",
+ props:["nombredeporte"],
+ mounted(){
+    console.log("Deporte recibido: "+this.nombredeporte);
+ }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,33 @@
+<template>
+  <HijoDeporte></HijoDeporte>
+  <div>
+    <h1 style="color:blue">Padre Deportes</h1>
+    <ul>
+      <li v-for="sport in deportes" :key="sport">
+        <HijoDeportes :nombredeporte="sport"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HijoDeporte from './HijoDeporte.vue'
+
+export default {
+  name:"PadreDeportes",
+  components:{
+    HijoDeporte
+  },
+  data(){
+  return{
+        deportes:["Curling","Esgrima","Hockey sobre hielo"]
+    }
+  }
+
+}
+</script>
+
+
+<style>
+
+</style>
